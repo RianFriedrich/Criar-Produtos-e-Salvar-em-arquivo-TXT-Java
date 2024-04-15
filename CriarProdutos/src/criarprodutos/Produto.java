e2f85755f5eb1c55324f42b62e95b180b61c9d68
RianFriedrich <125458446+RianFriedrich@users.noreply.github.com>
2024-04-15 10:02:13 -0300

    Add files via upload

---
+++ CriarProdutos/src/criarprodutos/Produto.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package criarprodutos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Produto{
+    private String CodBarra;
+    private String tipo;
+    private String Nome;
+    private String preco;
+    private String quantidade;
+    private String fabricante;
+    private String descricao;
+
+    public String getCodBarra() {
+        return CodBarra;
+    }
+
+    public void setCodBarra(String CodBarra) {
+        this.CodBarra = CodBarra;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+    
+    
+
+    
+    
+    
+    
+    
+}
